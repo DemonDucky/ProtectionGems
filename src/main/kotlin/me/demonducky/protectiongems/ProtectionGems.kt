ea815d3cafc8f13ecf1b487926a7294ceb1499a1
DemonDucky <cuti1mat@gmail.com>
2024-08-22 08:40:48 +0700

    First Commit

---
+++ src/main/kotlin/me/demonducky/protectiongems/ProtectionGems.kt
@@ -0,0 +1,14 @@
+package me.demonducky.protectiongems
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ProtectionGems : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
